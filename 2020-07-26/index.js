9c3ee10d40f1f71d142f31aa3eb94dbf1158fe68
liang.cheng <chengliang.chrome@gmail.com>
2020-07-27 15:22:40 +0800

    i

--- 2020-07-26/index.js
+++ 2020-07-26/index.js
@@ -1,9 +1,21 @@
-console.log(typeof true)
-console.log(typeof null)
-console.log(typeof undefined)
-console.log(typeof 123)
-console.log(typeof 123n)
-console.log(typeof 'abc')
-console.log(typeof Symbol('symbol'))
-console.log(typeof {a: 1, b: 2})
-console.log(typeof function name(params) {})
+// console.log(typeof true)
+// console.log(typeof null)
+// console.log(typeof undefined)
+// console.log(typeof 123)
+// console.log(typeof 123n)
+// console.log(typeof 'abc')
+// console.log(typeof Symbol('symbol'))
+// console.log(typeof {a: 1, b: 2})
+// console.log(typeof function name(params) {})
+
+// console.log(typeof {a: 'a', b: 'b'})
+// console.log(typeof Promise.resolve())
+// console.log(typeof new Date())
+// console.log(typeof [1, 2, 3])
+
+const _toString = Object.prototype.toString
+class A {
+
+}
+
+console.log(_toString.call(new A()))
